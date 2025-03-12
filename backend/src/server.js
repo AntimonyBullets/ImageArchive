b9edec55fb5f62508c51efba4a725df843928a2a
AntimonyBullets <antimonybullets@gmail.com>
2025-03-12 17:04:46 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+import {app} from './app.js';
+dotenv.config({
+    path: './.env'
+});
+
+const port = process.env.PORT || 5000;
+
+connectDB()
+.then(()=>{
+    app.listen(port, ()=>{
+        console.log(`Server is running at port ${port}`)
+    });
+})
+
